705bbc5a4de105bf514b4d429ead8d5e268fbcc3
Prabin Rag TK <prabin.rag@claysys.in>
2024-06-07 12:29:43 +0530

    Add project files.

---
+++ appreview/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Plugin.StoreReview;
+
+namespace appreview
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            await CrossStoreReview.Current.RequestReview(true);
+        }
+
+        async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            await CrossStoreReview.Current.RequestReview(false);
+        }
+    }
+}
